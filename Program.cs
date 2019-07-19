312ab88506b07716e0b3f10d19097bfd48c73397
Xinran <52697166+xrma99@users.noreply.github.com>
2019-07-19 17:22:05 +0800

    2019.9.19
    Read from the dll file in Hexadecimal.
    Print it out in Console.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Versioning;
+
+namespace CsharpDLLparser
+{
+    class Program
+    {
+        static void readfile(string filepath)
+        {
+            //读取二进制文件
+            FileStream fs = new FileStream("C:\\Users\\t-xinma\\Documents\\Csharp_tryout.dll",FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+            Byte content;
+            long totallen = br.BaseStream.Length;//total length
+
+            //写入txt文件
+            FileStream fr = new FileStream("C:\\Users\\t-xinma\\Documents\\test.txt", FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fr);
+
+            while (br.BaseStream.Position<br.BaseStream.Length)
+            {
+                content = br.ReadByte();
+                
+                Console.Write(content.ToString("X"));
+                sw.Write(content.ToString("X"));
+            }
+
+            fs.Close();
+            br.Close();
+
+            fr.Close();
+            br.Close();
+
+            
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input file path");
+            string filepath=Console.ReadLine();
+            readfile(filepath);
+        }
+    }
+}
